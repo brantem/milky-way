3b935147470b2baf6f167ec706c704302dbf06b3
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2023-12-08 21:29:27 +0700

    planets: auto scroll neptune

--- planets/src/components/planets/Neptune.tsx
+++ planets/src/components/planets/Neptune.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { subscribe } from 'valtio';
 
 import Moon from '../Moon';
@@ -8,19 +8,29 @@ import { points } from '../../lib/state';
 import { usePlanet } from '../../lib/hooks';
 
 const Neptune = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
   const planet = usePlanet<Neptune>();
 
   const [stopAt, setStopAt] = useState<number>();
+
+  const updateStopAt = (i: number) => {
+    setStopAt(i);
+    setTimeout(() => {
+      const el = containerRef.current?.querySelector(`& > div > [data-i="${i === -1 ? planet.moons.length - 1 : i}"]`);
+      if (!el) return;
+      el.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'center' });
+    }, 100);
+  };
 
   useEffect(() => {
     const cb = () => {
       for (let i = 0; i < planet.moons.length; i++) {
         const moon = planet.moons[i];
         if ((points.value[moon.id] || 0) >= (moon.points?.min || 0)) continue;
-        setStopAt(i);
+        updateStopAt(i);
         return;
       }
-      setStopAt(-1);
+      updateStopAt(-1);
     };
 
     cb();
@@ -29,11 +39,18 @@ const Neptune = () => {
 
   return (
     <div className="flex-1 p-3 flex overflow-hidden">
-      <div className="flex-1 py-5 overflow-y-auto [scrollbar-gutter:stable] rounded-lg bg-white border border-neutral-200/50 shadow-sm">
+      <div
+        ref={containerRef}
+        className="flex-1 py-5 overflow-y-auto [scrollbar-gutter:stable] rounded-lg bg-white border border-neutral-200/50 shadow-sm"
+      >
         <div className="max-w-5xl mx-auto flex flex-col items-center gap-5">
           {planet.moons.map((moon, i) => {
             if (stopAt === undefined || (stopAt !== -1 && i > stopAt)) return null;
-            return <Moon key={i} moon={moon} />;
+            return (
+              <div key={i} data-i={i}>
+                <Moon moon={moon} />
+              </div>
+            );
           })}
         </div>
       </div>
